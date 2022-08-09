b50f2df6e37fd5fe4d02c9e0c91fb73d7aac2e69
Dylan Duke <laytonmaes@gmail.com>
2022-08-09 09:32:52 -0600

    Add post

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,4 +1,26 @@
-export const getUrls = () => {
+export const getUrls = (urlBody) => {
   return fetch('http://localhost:3001/api/v1/urls')
-      .then(response => response.json())
+      .then(response => {
+        if(!response.ok) {
+          throw new Error(response.status + " " + response.statusText)
+        } else {
+          response.json()
+        }
+      })
+}
+
+export const postUrl = () => {
+  return fetch("/api/v1/urls",
+  {
+    method:"POST",
+    headers:{"Content-Type": "application/json"},
+    body:JSON.stringify(urlBody)
+  })
+  .then(response => {
+    if(!response.ok) {
+      throw new Error(response.status + " " + response.statusText)
+    } else {
+      response.json()
+    }
+  })
 }
